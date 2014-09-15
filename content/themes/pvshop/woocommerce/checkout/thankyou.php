f7a59180c213fe4a4b5be204e16abdf250183779
gui machiavelli <hey@guimachiavelli.com>
2014-09-15 19:02:38 -0300

    thank you page styles

--- content/themes/pvshop/woocommerce/checkout/thankyou.php
+++ content/themes/pvshop/woocommerce/checkout/thankyou.php
@@ -10,60 +10,59 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $woocommerce;
+?>
 
+<div class="cabinet cabinet--checkout cabinet--thankyou cabinet-top">
+    <div class="cabinet-body">
+<?php
 if ( $order ) : ?>
 
-	<?php if ( in_array( $order->status, array( 'failed' ) ) ) : ?>
+    <?php if ( in_array( $order->status, array( 'failed' ) ) ) : ?>
 
-		<p><?php _e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction.', 'woocommerce' ); ?></p>
+        <p><?php _e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction.', 'woocommerce' ); ?></p>
 
-		<p><?php
-			if ( is_user_logged_in() )
-				_e( 'Please attempt your purchase again or go to your account page.', 'woocommerce' );
-			else
-				_e( 'Please attempt your purchase again.', 'woocommerce' );
-		?></p>
+    <?php else : ?>
 
-		<p>
-			<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php _e( 'Pay', 'woocommerce' ) ?></a>
-			<?php if ( is_user_logged_in() ) : ?>
-			<a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'myaccount' ) ) ); ?>" class="button pay"><?php _e( 'My Account', 'woocommerce' ); ?></a>
-			<?php endif; ?>
-		</p>
+        <p class="pannel">Your order has been received. You'll now receive an email from us.</p>
+        <div class="table-wrapper">
+            <table class="order_details">
+                <thead>
+                    <tr>
+                        <th class="order">Order</th>
+                        <th class="date">Date</th>
+                        <th class="total">Total</th>
+                        <th class="method">Payment Method</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <strong><?php echo $order->get_order_number(); ?></strong>
+                        </td>
+                        <td class="date">
+                            <strong><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></strong>
+                        </td>
+                        <td class="total">
+                            <strong><?php echo $order->get_formatted_order_total(); ?></strong>
+                        </td>
+                        <td class="method">
+                            <strong><?php echo $order->payment_method_title; ?></strong>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    <?php endif; ?>
 
-	<?php else : ?>
-
-		<p><?php _e( 'Thank you. Your order has been received.', 'woocommerce' ); ?></p>
-
-		<ul class="order_details">
-			<li class="order">
-				<?php _e( 'Order:', 'woocommerce' ); ?>
-				<strong><?php echo $order->get_order_number(); ?></strong>
-			</li>
-			<li class="date">
-				<?php _e( 'Date:', 'woocommerce' ); ?>
-				<strong><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></strong>
-			</li>
-			<li class="total">
-				<?php _e( 'Total:', 'woocommerce' ); ?>
-				<strong><?php echo $order->get_formatted_order_total(); ?></strong>
-			</li>
-			<?php if ( $order->payment_method_title ) : ?>
-			<li class="method">
-				<?php _e( 'Payment method:', 'woocommerce' ); ?>
-				<strong><?php echo $order->payment_method_title; ?></strong>
-			</li>
-			<?php endif; ?>
-		</ul>
-		<div class="clear"></div>
-
-	<?php endif; ?>
-
-	<?php do_action( 'woocommerce_thankyou_' . $order->payment_method, $order->id ); ?>
-	<?php do_action( 'woocommerce_thankyou', $order->id ); ?>
+    <div class="bank-account">
+        <?php do_action( 'woocommerce_thankyou_' . $order->payment_method, $order->id ); ?>
+    </div>
 
 <?php else : ?>
 
-	<p><?php _e( 'Thank you. Your order has been received.', 'woocommerce' ); ?></p>
+    <p><?php _e( 'Thank you. Your order has been received.', 'woocommerce' ); ?></p>
 
 <?php endif; ?>
+    </div>
+    <div class="cabinet-bottom"></div>
+</div>
