4026221328e978847ef608e969d8fe14f34cef6e
gui machiavelli <hey@guimachiavelli.com>
2014-10-03 21:34:18 +0100

    target blank on footer links

--- content/themes/pvshop/footer.php
+++ content/themes/pvshop/footer.php
@@ -1,11 +1,11 @@
 		</main>
 
         <p class="site-footer">
-			<a href="http://www.pinar-viola.com/">Pinar&Viola</a>
+			<a target="_blank" href="http://www.pinar-viola.com/">Pinar&Viola</a>
 			– All inquiries: priscilla@pinar-viola.com –
-            <a href="http://facebook.com/PinarViola">Facebook</a> –
-            <a href="http://instagram.com/pinar_viola">Instagram</a> –
-            <a href="http://twitter.com/Pinar_Viola">Twitter</a>
+            <a target="_blank" href="http://facebook.com/PinarViola">Facebook</a> –
+            <a target="_blank" href="http://instagram.com/pinar_viola">Instagram</a> –
+            <a target="_blank" href="http://twitter.com/Pinar_Viola">Twitter</a>
         </p>
 
 
