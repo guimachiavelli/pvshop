c01ae398fa47bf781d9823350cd90a21fad430e3
gui machiavelli <hey@guimachiavelli.com>
2014-10-08 21:00:54 +0100

    removed prices from front page

--- content/themes/pvshop/inc/woocommerce/archive-product.php
+++ content/themes/pvshop/inc/woocommerce/archive-product.php
@@ -7,6 +7,8 @@
 
 	remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
 	remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
+	remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
+
 
 	remove_all_actions('woocommerce_before_shop_loop', 30);
 	remove_all_actions('woocommerce_after_shop_loop_item');
